72326b85c731eceb53c09e90d9fea61608ce123d
Jeff LaRocque <j.larocque001@gmail.com>
2019-04-14 15:49:50 -0400

    untested proxy

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/:id', express.static(path.join(__dirname, '/public')));
+app.use(cors());
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
